d43fdf5dc8f8d84d10faf57f81df8d5fc4c7d41c
juskins <omojuwababatunde1@gmail.com>
2026-01-18 22:48:18 +0100

    Implementing Advanced Model Relationships in Django

--- Introduction_to_Django/LibraryProject/LibraryProject/urls.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bookshelf.urls')),
+    path('relationship/', include('relationship_app.urls')),
 ]
